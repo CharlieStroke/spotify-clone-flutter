4366000faa15eabc26c0fcdd46bbd766beb47e99
CharlieStroke <stroke.24.13.16@gmail.com>
2026-02-22 15:48:04 -0600

    Implement album CRUD management features

--- backend/src/routes/albumRoutes.js
+++ backend/src/routes/albumRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { createAlbum } = require('../controllers/albumController');
+const { createAlbum, getAlbumsByArtist, deleteAlbum, updateAlbumName } = require('../controllers/albumController');
 const authenticateToken = require('../middleware/authMiddleware');
 
+
 router.post('/create', authenticateToken, createAlbum);
+router.get('/my-albums', authenticateToken, getAlbumsByArtist);
+router.put('/update/:id', authenticateToken, updateAlbumName);
+router.delete('/delete/:id', authenticateToken, deleteAlbum);
+
 
 module.exports = router;
