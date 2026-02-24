a8a5b12da0c17a71fc2115706c15d3f9d23a2f37
CharlieStroke <stroke.24.13.16@gmail.com>
2026-02-23 23:58:09 -0600

    Refactor OCI configuration: switch to Instance Principals authentication and remove Object Storage client initialization

--- backend/src/config/oci.js
+++ backend/src/config/oci.js
@@ -1,13 +1,5 @@
 const common = require("oci-common");
-const objectStorage = require("oci-objectstorage");
 
-const provider = new common.ConfigFileAuthenticationDetailsProvider(
-    "C:/Users/Carlos/.oci/config", // Path to your OCI config file
-    "DEFAULT"
-);
+const provider = new common.InstancePrincipalsAuthenticationDetailsProvider();
 
-const client = new objectStorage.ObjectStorageClient({
-    authenticationDetailsProvider: provider
-});
-
-module.exports = client;
+module.exports = provider;
