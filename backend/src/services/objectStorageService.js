e23c2fc776df28a0334e3130a7bd349c6bd9a151
CharlieStroke <stroke.24.13.16@gmail.com>
2026-02-24 00:11:19 -0600

    Refactor OCI configuration: update getProvider function to use builder pattern and improve client initialization

--- backend/src/services/objectStorageService.js
+++ backend/src/services/objectStorageService.js
@@ -1,23 +1,35 @@
-const client = require('../config/oci');
-const common = require('oci-common');
+const objectstorage = require("oci-objectstorage");
+const getProvider = require("../config/oci");
 
-const namespaceName = 'axeteiujatw7';
-const bucketName = 'music-spotify-clone-bucket';
+const namespaceName = "axeteiujatw7";
+const bucketName = "music-spotify-clone-bucket";
 
-const uploadFile = async (fileBuffer, objectName, contentType) => {
+async function getClient() {
+    const provider = await getProvider();
+
+    const client = new objectstorage.ObjectStorageClient({
+        authenticationDetailsProvider: provider
+    });
+
+    return client;
+    }
+
+    const uploadFile = async (fileBuffer, objectName, contentType) => {
+    const client = await getClient();
+
     const putObjectRequest = {
-        namespaceName: namespaceName,
-        bucketName: bucketName,
-        objectName: objectName,
+        namespaceName,
+        bucketName,
+        objectName,
         putObjectBody: fileBuffer,
-        contentType: contentType
+        contentType
     };
 
     await client.putObject(putObjectRequest);
+
     return `https://objectstorage.${process.env.OCI_REGION}.oraclecloud.com/n/${namespaceName}/b/${bucketName}/o/${objectName}`;
+    };
 
-};
-
-module.exports = {
+    module.exports = {
     uploadFile
 };
