3d931a9339b08172bd199cd2d2d3520ca3ae3e97
CharlieStroke <stroke.24.13.16@gmail.com>
2026-03-01 16:22:48 -0600

    define domain repository interface, implementation and authentication use cases

---
+++ lib/features/auth/domain/repository/auth_repository_impl.dart
@@ -0,0 +1,30 @@
+import 'package:dartz/dartz.dart';
+import '../../domain/repository/auth_repository.dart';
+import '../../domain/entities/user_entity.dart';
+import '/features/auth/data/sources/auth_api_service.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthApiService authApiService;
+
+  AuthRepositoryImpl(this.authApiService);
+
+  @override
+  Future<Either<String, UserEntity>> register(String email, String password, String name) async {
+    try {
+      final user = await authApiService.register(email, password, name);
+      return Right(user); // Éxito
+    } catch (e) {
+      return Left(e.toString()); // Error
+    }
+  }
+
+  @override
+  Future<Either<String, UserEntity>> login(String email, String password) async {
+    try {
+      final user = await authApiService.login(email, password);
+      return Right(user);
+    } catch (e) {
+      return Left(e.toString());
+    }
+  }
+}
