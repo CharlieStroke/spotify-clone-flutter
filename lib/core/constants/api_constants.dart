be40b4f003540b218133c018ae6c0ab816b57a39
CharlieStroke <stroke.24.13.16@gmail.com>
2026-03-01 15:39:50 -0600

    Setup core architecture including theme, colors, api constants and dio network client

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,10 @@
+class ApiConstants {
+  static const String baseUrl = 'http://159.54.145.22:3000/api';
+
+
+  static const String loginEndpoint = '/auth/login';
+  static const String registerEndpoint = '/auth/register';
+  static const String userProfileEndpoint = '/auth/';
+
+
+}
